85e77b2cd870614278c7b7ab498f5ba524c00b0b
Karim Khoja <karimkhoja13@yahoo.com>
2025-12-30 16:20:58 -0800

    added example state (firstName) to test out Zustand in my app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,42 @@
 import { useState } from 'react';
 import { TaskList } from './pages/TaskList';
 import { Project } from './pages/Project';
+import { create } from 'zustand';
+import type { Task } from './components/TaskItem';
+
+// 1. Define the shape of a single Task
+type Task = {
+  name: string;
+  completedStatus: boolean;
+  project: string;
+};
+
+type State = {
+  firstName: string;
+  tasks: Task[];
+};
+
+// 3. Define the Actions (functions)
+type Action = {
+  updateFirstName: (firstName: State['firstName']) => void;
+  addTask: (newTask: Task) => void;
+};
+
+// 4. Create the store
+export const useTaskStore = create<State & Action>((set) => ({
+  firstName: '',
+  // Initial State
+  tasks: [
+    { name: 'Finish homework', completedStatus: true, project: 'Work' },
+    { name: 'Clean room', completedStatus: false, project: 'Personal' },
+    { name: 'Grocery shopping', completedStatus: true, project: 'Work' },
+    { name: 'Study React', completedStatus: false, project: 'Work' },
+  ],
+
+  // Actions
+  updateFirstName: (firstName) => set(() => ({ firstName: firstName })),
+  addTask: (newTask) => set((state) => ({ tasks: [...state.tasks, newTask] })),
+}));
 
 const taskItems = [
   { name: 'Finish homework', completedStatus: true, project: 'Work' },
@@ -52,9 +88,28 @@ export default function App() {
     (task) => task.project === selectedProject
   );
 
+  // "select" the needed state and actions, in this case, the firstName value
+  // and the action updateFirstName
+  const firstName = useTaskStore((state) => state.firstName);
+  const updateFirstName = useTaskStore((state) => state.updateFirstName);
+
   return (
     <>
-    <p>{tasks.length}</p>
+      <main>
+        <label>
+          First name
+          <input
+            // Update the "firstName" state
+            onChange={(e) => updateFirstName(e.currentTarget.value)}
+            value={firstName}
+          />
+        </label>
+
+        <p>
+          Hello, <strong>{firstName}!</strong>
+        </p>
+      </main>
+      <p>{tasks.length}</p>
       {page === 'project' && <Project allTasks={tasks} goToTasks={goToTasks} />}
       {page === 'tasks' && (
         <TaskList
